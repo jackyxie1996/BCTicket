86fe98ac83b4b4f1f36d7c378a6367bb17a81218
You Zhou <yzhou0528@gmail.com>
2024-08-22 22:18:33 -0700

    add code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # BCTicket
+1. 打开terminal(Macbook)或者cmd(Windows)
+2. 运行 `git clone https://github.com/ZhouYou528/BCTicket.git`
+3. 运行 `cd BCTicket`
+4. 运行 `npm install`
+5. 运行 `npm run cypress:open`
+6. 弹出窗口中点击 `E2E Testing`
+7. 点击 `Start E2E Testing in Chrome`
+8. 点击 `ticket.cy.js`
+9. 窗口中会自动选择joffre lake然后自动填写姓名邮箱人数，最后的验证码需要手动输入。最后手动点击确定。
---
+++ cypress/e2e/ticket.cy.js
@@ -0,0 +1,35 @@
+describe('template spec', () => {
+  // change date here
+  const DATE = '12';
+  // change number of people here
+  const NUM_PPL = '4';
+  // change to your own name and email
+  const FIRST_NAME = 'FirstName';
+  const LAST_NAME = 'LastName';
+  const EMAIL = 'xxxx@gmail.com';
+
+  // const FIRST_NAME = 'Another1';
+  // const LAST_NAME = 'Another1';
+  // const EMAIL = 'xxxxx@gmail.com';
+
+  // const FIRST_NAME = 'Another2';
+  // const LAST_NAME = 'Another2';
+  // const EMAIL = 'xxxxx@gmail.com';
+
+  it('load page', () => {
+    cy.viewport(2000, 1750);
+    cy.visit('https://reserve.bcparks.ca/dayuse');
+    cy.get(`[aria-label="Book a pass for Joffre Lakes Provincial Park"]`).click();
+    cy.get(`[title="Select a Date"]`).click();
+    cy.get('.btn-light').contains(new RegExp("^" + DATE + "$", "g")).click();
+    cy.get('#passType').select('Joffre Lakes - Trail');
+    cy.get('.card-input').click();
+    cy.get('#passCount').select(NUM_PPL);
+    cy.get('.btn').contains('Next').click();
+    cy.get('#firstName').type(FIRST_NAME);
+    cy.get('#lastName').type(LAST_NAME);
+    cy.get('#email').type(EMAIL);
+    cy.get('#emailCheck').type(EMAIL);
+    cy.get('[type="checkbox"]').check();
+  })
+})
